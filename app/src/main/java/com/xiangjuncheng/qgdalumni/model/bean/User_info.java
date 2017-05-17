63941f4fb2bb8621e8cb19e736b905d4968be2d7
xiangjuncheng <xiangjuncheng68@163.com>
2017-05-17 20:51:49 +0800

    Supplementary submission

---
+++ app/src/main/java/com/xiangjuncheng/qgdalumni/model/bean/User_info.java
@@ -0,0 +1,35 @@
+package com.xiangjuncheng.qgdalumni.model.bean;
+
+import cn.bmob.v3.BmobUser;
+
+public class User_info extends BmobUser implements java.io.Serializable {
+    String name;
+    String collage;
+    int year;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCollage() {
+        return collage;
+    }
+
+    public void setCollage(String collage) {
+        this.collage = collage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
